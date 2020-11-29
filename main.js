f67279a30f742071c41308f3299a6483179c1ac2
Miguel-Concha <miguel@deeplegal.ai>
2020-11-29 19:48:10 -0300

    funcion elementos que caen

--- main.js
+++ main.js
@@ -1,23 +1,28 @@
 const backgroundWidth = document.getElementById("background").offsetWidth
-let leftValue = backgroundWidth/2
-let verticalPosition = 0
-const moveElement = 10
 const move = 35
+let leftValue = backgroundWidth/2
+let numberOfElements = 10
+let elementsFrecuency = 5000
+
+function createElement() {
+  const imageNumber = Math.round((Math.random() * 33) + 1)
+  let element = document.createElement("div")
+  element.className = "element"
+  element.style.backgroundImage = 'url("img/' + imageNumber + '.png")'
+  element.style.left = (Math.random() * (backgroundWidth - 100)) + "px"
+  document.getElementById('background').appendChild(element)
+}
 
 document.onkeydown = function(e){
   if(e.key == 'ArrowRight' && leftValue < backgroundWidth - 86) {
-    leftValue = leftValue + move;
-    document.getElementById('character').style.left = leftValue+"px";
+    leftValue = leftValue + move
+    document.getElementById('character').style.left = leftValue+"px"
   } else if(e.key == 'ArrowLeft' && leftValue > 0) {
-    leftValue = leftValue - move;
-    document.getElementById('character').style.left = leftValue+"px";
-  } else if(e.key == 'ArrowUp') {
-    verticalPosition = verticalPosition - moveElement;
-    document.getElementById('element').style.top = verticalPosition+"px";
-  } else if(e.key == 'ArrowDown') {
-    verticalPosition = verticalPosition + moveElement;
-    document.getElementById('element').style.top = verticalPosition+"px";
+    leftValue = leftValue - move
+    document.getElementById('character').style.left = leftValue+"px"
   }
-
 }
 
+for (let i = 0; i < numberOfElements; i++) {
+  setTimeout(() => { createElement() }, elementsFrecuency);
+}
